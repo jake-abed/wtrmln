b34ae08fea97144053d3f925b1cdf88948e8fd68
Jake Abed <jakeabed@gmail.com>
2024-01-18 20:39:48 -0800

    Tidy up dev.exs and runtime.exs for deploy

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,13 +48,13 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  _host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :wtrmln, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :wtrmln, WtrmlnWeb.Endpoint,
-    url: [host: host, port: port, scheme: "https"],
+    url: [host: "wtrmln.chat", port: port, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
